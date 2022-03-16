1ec056e70bc2feb707b92e1f30cf570ffe315dfe
Demin Dmitry <heaven7pro@gmail.com>
2022-03-16 13:01:45 +0300

    Step 2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,57 @@
+import onChange from 'on-change';
+import { string } from 'yup';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
+import './style.css'; // todo ?
+import { renderForm, renderList } from './view';
+
+const validateUrl = (url) => {
+  const schema = string().url();
+  return schema.validate(url);
+};
+
+const app = () => {
+  const state = {
+    urls: [],
+    ui: {
+      form: {
+        state: null,
+        error: null,
+      },
+    },
+  };
+  const watchedState = onChange(state, (path) => {
+    // todo remove debug
+    // console.log(JSON.stringify(state, null, 2));
+
+    if (path === 'ui.form.state') {
+      renderForm(state);
+    } else if (path === 'urls') {
+      renderList(state.urls);
+    }
+  });
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const url = data.get('url').trim();
+    watchedState.ui.form.state = 'validating';
+    validateUrl(url)
+      .then(() => {
+        if (watchedState.urls.includes(url)) {
+          watchedState.ui.form.error = 'Эта ссылка уже была добавлена ранее';
+          watchedState.ui.form.state = 'error';
+          return;
+        }
+        watchedState.urls.push(url);
+        watchedState.ui.form.error = null;
+        watchedState.ui.form.state = 'ready';
+      })
+      .catch(() => {
+        watchedState.ui.form.error = 'Невалидная ссылка';
+        watchedState.ui.form.state = 'error';
+      });
+  });
+};
+
+app();
---
+++ src/view.js
@@ -0,0 +1,43 @@
+const renderForm = (state) => {
+  const form = document.querySelector('form');
+  const input = form.querySelector('#url');
+  const feedback = form.querySelector('.invalid-feedback');
+  const button = form.querySelector('button');
+
+  switch (state.ui.form.state) {
+    case 'ready': {
+      input.classList.remove('is-invalid');
+      feedback.textContent = '';
+      form.reset();
+      input.focus();
+      button.disabled = false;
+      break;
+    }
+    case 'validating':
+    case 'processing': {
+      button.disabled = true;
+      break;
+    }
+    case 'error': {
+      input.classList.add('is-invalid');
+      feedback.textContent = state.ui.form.error;
+      button.disabled = false;
+      break;
+    }
+    default:
+      throw new Error(`Untracked UI state: ${state.ui.form.state}`);
+  }
+};
+
+const renderList = (urls) => {
+  const list = document.querySelector('#list');
+  const ul = document.createElement('ul');
+  urls.forEach((url) => {
+    const li = document.createElement('li');
+    li.textContent = url;
+    ul.append(li);
+  });
+  list.replaceChildren(ul);
+};
+
+export { renderForm, renderList };
