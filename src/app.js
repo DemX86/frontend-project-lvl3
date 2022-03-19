74f4803faad9e70e3ec3239398142cf19c026af2
Demin Dmitry <heaven7pro@gmail.com>
2022-03-19 20:59:10 +0300

    Make a separate entry point for tests

---
+++ src/app.js
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import i18next from 'i18next';
+import onChange from 'on-change';
+
+import resources from './locales/index.js';
+import loadFeed from './loader/index.js';
+import updateFeedsBg from './updater.js';
+import { renderFeeds, renderForm, renderPosts } from './view.js';
+
+import 'bootstrap';
+
+const app = () => {
+  const defaultLanguage = 'ru';
+
+  const i18 = i18next.createInstance();
+  i18.init({
+    lng: defaultLanguage,
+    resources,
+  });
+
+  const ax = axios.create();
+
+  const state = {
+    lng: defaultLanguage,
+    feeds: [],
+    posts: [],
+    postsRead: [],
+    ui: {
+      form: {
+        state: 'start',
+        error: null,
+      },
+    },
+  };
+  const watchedState = onChange(state, (path, value) => {
+    if (path === 'ui.form.state') {
+      renderForm(i18, state.ui.form);
+    } else if (path === 'feeds') {
+      renderFeeds(i18, state.feeds);
+    } else if (path === 'posts') {
+      renderPosts(i18, state.posts, state.postsRead);
+    } else if (path === 'lng') {
+      i18.changeLanguage(value)
+        .then(() => {
+          renderForm(i18, state.ui.form);
+        });
+    }
+  });
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (event) => {
+    loadFeed(event, i18, ax, watchedState);
+  });
+
+  const lngSelector = document.querySelector('#lng-selector');
+  lngSelector.addEventListener('click', (event) => {
+    watchedState.ui.form.state = 'start';
+    watchedState.lng = event.target.dataset.lng;
+  });
+
+  renderForm(i18, state.ui.form);
+
+  updateFeedsBg(i18, ax, watchedState);
+};
+
+export default app;
