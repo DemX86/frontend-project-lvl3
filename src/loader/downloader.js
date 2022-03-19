1c703db2199d5c338e25f0ee3940fb2028580f09
Demin Dmitry <heaven7pro@gmail.com>
2022-03-19 22:14:26 +0300

    Update network error handling

--- src/loader/downloader.js
+++ src/loader/downloader.js
@@ -6,9 +6,9 @@ const downloadXml = (i18, ax, url) => {
   const proxiedUrl = generateProxiedUrl(url);
   return ax.get(proxiedUrl)
     .then((rs) => {
-      // if (rs.data.status.error) {
-      //   return Promise.reject(Error(i18.t('form.errors.failedLoading')));
-      // }
+      if (rs.statusText !== 'OK') {
+        return Promise.reject(Error(i18.t('form.errors.failedLoading')));
+      }
       return rs.data.contents;
     });
 };
