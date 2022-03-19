8438d015a5017c4fbaa98a197dce30bcf88a33e0
Demin Dmitry <heaven7pro@gmail.com>
2022-03-19 21:57:29 +0300

    Debug

--- src/loader/downloader.js
+++ src/loader/downloader.js
@@ -6,10 +6,9 @@ const downloadXml = (i18, ax, url) => {
   const proxiedUrl = generateProxiedUrl(url);
   return ax.get(proxiedUrl)
     .then((rs) => {
-      console.log('RS:', rs);
-      if (rs.data.status.error) {
-        return Promise.reject(Error(i18.t('form.errors.failedLoading')));
-      }
+      // if (rs.data.status.error) {
+      //   return Promise.reject(Error(i18.t('form.errors.failedLoading')));
+      // }
       return rs.data.contents;
     });
 };
