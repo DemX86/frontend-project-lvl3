0dbf4e079a432c9be60ba1df3027741b34e95b7b
Demin Dmitry <heaven7pro@gmail.com>
2022-03-24 13:18:18 +0300

    Fix 1.4 — Create new URL with JS build-in URL constructor

--- src/controller/loader/downloader.js
+++ src/controller/loader/downloader.js
@@ -1,6 +1,6 @@
 const PROXY_URL = 'https://allorigins.hexlet.app/get?disableCache=true&url=';
 
-const generateProxiedUrl = (url) => `${PROXY_URL}${encodeURIComponent(url)}`;
+const generateProxiedUrl = (url) => new URL(`${PROXY_URL}${encodeURIComponent(url)}`);
 
 const downloadXml = (i18, ax, url) => {
   const proxiedUrl = generateProxiedUrl(url);
