f9157d1b86761f8d484a3da6837fb79c495e7e3f
Demin Dmitry <heaven7pro@gmail.com>
2022-03-27 11:00:00 +0300

    Init i18next asynchronously

--- src/app.js
+++ src/app.js
@@ -14,75 +14,76 @@ const app = () => {
   i18.init({
     lng: defaultLanguage,
     resources,
-  });
-
-  const state = {
-    lng: defaultLanguage,
-    feeds: [],
-    posts: [],
-    postReadIds: [],
-    ui: {
-      form: {
-        state: 'start',
-        error: null,
-      },
-      modal: {
-        loadedPostId: null,
-      },
-    },
-  };
-  const watchedState = onChange(state, (path, value) => {
-    switch (path) {
-      case 'lng': {
-        i18.changeLanguage(value)
-          .then(() => {
+  })
+    .then(() => {
+      const state = {
+        lng: defaultLanguage,
+        feeds: [],
+        posts: [],
+        postReadIds: [],
+        ui: {
+          form: {
+            state: 'start',
+            error: null,
+          },
+          modal: {
+            loadedPostId: null,
+          },
+        },
+      };
+      const watchedState = onChange(state, (path, value) => {
+        switch (path) {
+          case 'lng': {
+            i18.changeLanguage(value)
+              .then(() => {
+                view.renderForm(i18, state.ui.form);
+              });
+            break;
+          }
+          case 'feeds': {
+            view.renderFeeds(i18, state.feeds);
+            break;
+          }
+          case 'posts': {
+            view.renderPosts(i18, state.posts);
+            view.markReadPosts(state.postReadIds);
+            break;
+          }
+          case 'postReadIds': {
+            view.markReadPosts(state.postReadIds);
+            break;
+          }
+          case 'ui.form.state': {
             view.renderForm(i18, state.ui.form);
-          });
-        break;
-      }
-      case 'feeds': {
-        view.renderFeeds(i18, state.feeds);
-        break;
-      }
-      case 'posts': {
-        view.renderPosts(i18, state.posts);
-        view.markReadPosts(state.postReadIds);
-        break;
-      }
-      case 'postReadIds': {
-        view.markReadPosts(state.postReadIds);
-        break;
-      }
-      case 'ui.form.state': {
-        view.renderForm(i18, state.ui.form);
-        break;
-      }
-      case 'ui.modal.loadedPostId': {
-        view.prepareModal(i18, state);
-        break;
-      }
-      // no default
-    }
-  });
+            break;
+          }
+          case 'ui.modal.loadedPostId': {
+            view.prepareModal(i18, state);
+            break;
+          }
+          // no default
+        }
+      });
 
-  const form = document.querySelector('form');
-  form.addEventListener('submit', (event) => {
-    controller.loadFeed(event, watchedState);
-  });
+      const form = document.querySelector('form');
+      form.addEventListener('submit', (event) => {
+        controller.loadFeed(event, watchedState);
+      });
 
-  const postsContainer = document.querySelector('#posts');
-  postsContainer.addEventListener('click', (event) => {
-    controller.handlePostActions(event, watchedState);
-  });
+      const postsContainer = document.querySelector('#posts');
+      postsContainer.addEventListener('click', (event) => {
+        controller.handlePostActions(event, watchedState);
+      });
 
-  const lngSelector = document.querySelector('#lng-selector');
-  lngSelector.addEventListener('click', (event) => {
-    controller.changeLanguage(event, watchedState);
-  });
+      const lngSelector = document.querySelector('#lng-selector');
+      lngSelector.addEventListener('click', (event) => {
+        controller.changeLanguage(event, watchedState);
+      });
 
-  controller.updateFeedsBg(watchedState);
+      controller.updateFeedsBg(watchedState);
 
-  view.renderForm(i18, state.ui.form);
+      view.renderForm(i18, state.ui.form);
+    });
 };
 
 export default app;
